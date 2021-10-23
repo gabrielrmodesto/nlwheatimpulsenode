2474d890161ddbd5ed4b445c581ac73d6c6ebe45
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-10-23 06:47:49 -0300

    Busca do perfil do usuario

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 import { CreateMessageController } from "./controllers/CreateMessageController";
 import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
 import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const routes = Router();
@@ -14,4 +15,6 @@ routes.post(
 	new CreateMessageController().handle
 );
 routes.get("/messages/last3", new GetLast3MessagesController().handle);
+
+routes.get("/profile", ensureAuthenticated, new ProfileUserController().handle);
 export { routes };
