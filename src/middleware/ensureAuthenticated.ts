8ee40d58020d766f2feacec634594a87cfd47287
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-10-23 05:54:52 -0300

    Criado a mensagem

--- src/middleware/ensureAuthenticated.ts
+++ src/middleware/ensureAuthenticated.ts
@@ -2,34 +2,35 @@ import { Request, Response, NextFunction } from "express";
 import { verify } from "jsonwebtoken";
 
 interface IPayload {
-	sub: string;
+  sub: string;
 }
 
 export function ensureAuthenticated(
-	request: Request,
-	response: Response,
-	next: NextFunction
+  request: Request,
+  response: Response,
+  next: NextFunction
 ) {
-	const authToken = request.headers.authorization;
-	if (!authToken) {
-		return response.status(401).json({
-			errorCode: "token.invalid",
-		});
-	}
+  const authToken = request.headers.authorization;
 
-	//bearer 1234565
-	//[0] bearer
-	//[1] 123456
+  if (!authToken) {
+    return response.status(401).json({
+      errorCode: "token.invalid",
+    });
+  }
 
-	const [, token] = authToken.split(" ");
+  //Bearer 8934589345djisdjfk834u25ndsfksdkf
+  // [0] Bearer
+  // [1] 8934589345djisdjfk834u25ndsfksdkf
 
-	try {
-		const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+  const [, token] = authToken.split(" ");
 
-		request.user_id = sub;
-	} catch (error) {
-		return response.status(401).json({
-			errorCode: "token.expired",
-		});
-	}
+  try {
+    const { sub } = verify(token, process.env.JWT_SECRET) as IPayload;
+
+    request.user_id = sub;
+
+    return next();
+  } catch (err) {
+    return response.status(401).json({ errorCode: "token.expired" });
+  }
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
 
 const routes = Router();
 
 routes.post("/authenticate", new AuthenticateUserController().handle);
+
+routes.post(
+	"/message",
+	ensureAuthenticated,
+	new CreateMessageController().handle
+);
 
 export { routes };
