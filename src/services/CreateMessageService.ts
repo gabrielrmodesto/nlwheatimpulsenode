123037bfeba34038520ec002c33e19a5d2b07158
Gabriel Modesto <gabrielrmodesto5@gmail.com>
2021-10-23 05:30:06 -0300

    middleware para user

--- src/services/CreateMessageService.ts
+++ src/services/CreateMessageService.ts
@@ -1,7 +1,19 @@
-class CreateMessageService{
-	async execute(text: string, user_id: string){
+import prismaClient from "../prisma";
 
+class CreateMessageService {
+	async execute(text: string, user_id: string) {
+		const message = await prismaClient.message.create({
+			data: {
+				text,
+				user_id,
+			},
+			include: {
+				user: true,
+			},
+		});
+
+		return message;
 	}
 }
 
-export {CreateMessageService}
+export { CreateMessageService };
